43929698df86b3c212abcb2ee832a922a5327069
lorranyoliveira <lolo.souza80@yahoo.com>
2022-01-30 19:29:59 -0300

    Modifica arquivo snake e cria arquivo de entrada Co-authored-by: daniso123<dannysoaresdeoliveira@gmail.com>

--- snake.js
+++ snake.js
@@ -1,22 +1,19 @@
- export const SNAKE_SPEED=1
+import { getInputDirection } from "./input.js"
+
+ export const SNAKE_SPEED=4
  const snakeBody = [
      {x: 10, y: 11},
-     {x: 11, y: 11},
-     {x: 12, y: 11},
-     {x: 13, y: 11},
-     {x: 14, y: 11},
-
-
 ]
 
  export function update(){
+    const inputDirection = getInputDirection()
     for(let i=snakeBody.length -2; i>=0; i--){
         snakeBody[i + 1] = {...snakeBody[i]}
     }
-    snakeBody[0].x += 0
-    snakeBody[0].y += 1
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
  }
- export function draw(){
+ export function draw(gameBoard){
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div')
         snakeElement.style.gridRowStart = segment.x
