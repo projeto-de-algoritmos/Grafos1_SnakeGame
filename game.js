eeda37733946ed9ce014f6f4fc22de66e9eb13e0
Lorrany <lolo.souza80@yahoo.com>
2022-02-02 15:35:35 -0300

    Adiciona arquivo da comida e modifica a cobrinha. Co-authored-by: daniso123<dannysoaresdeoliveira@gmail.com>

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 import {SNAKE_SPEED, update as updateSnake, draw as drawSnake} from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
 
 let lastRenderTime=0
 const gameBoard = document.getElementById('game-board')
@@ -19,9 +20,11 @@ window.requestAnimationFrame(main)
 
 function update(){
     updateSnake()
+    updateFood()
 }
 
 function draw(){
     gameBoard.innerHTML = ''
     drawSnake(gameBoard)
+    drawFood(gameBoard)
 }
